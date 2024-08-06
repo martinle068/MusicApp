dc68384d2e8ea346a380578ce6d27834204b7645
Le Duc Hung <leduchung068@gmail.com>
2024-08-06 12:10:40 +0200

    Add MySong class

--- MusicApp/MusicApp/Models/YoutubeService.cs
+++ MusicApp/MusicApp/Models/YoutubeService.cs
@@ -5,9 +5,47 @@ using YouTubeMusicAPI.Client;
 using YouTubeMusicAPI.Models;
 using YoutubeExplode;
 using YoutubeExplode.Videos.Streams;
+using YouTubeMusicAPI.Models.Shelf;
+using System.Windows.Media.Imaging;
 
 namespace MusicApp.Models
 {
+	//string name,
+
+	//string id,
+	//ShelfItem[] artists,
+ //   ShelfItem album,
+	//TimeSpan duration,
+ //   bool isExplicit,
+
+	//string playsInfo,
+	//Radio radio,
+	public class MySong
+	{
+		public string Name { get; }
+		public string Id { get; }
+		public ShelfItem[] Artists { get; }
+		public ShelfItem Album { get; }
+		public TimeSpan Duration { get; }
+		public bool IsExplicit { get; }
+		public string PlaysInfo { get; }
+		public Radio Radio { get; }
+		public BitmapImage Thumbnail { get; set; }
+
+		public MySong(Song song)
+		{
+			Name = song.Name;
+			Id = song.Id;
+			Artists = song.Artists;
+			Album = song.Album;
+			Duration = song.Duration;
+			IsExplicit = song.IsExplicit;
+			PlaysInfo = song.PlaysInfo;
+			Radio = song.Radio;
+			Thumbnail = ThumbnailHelper.GetLowQualityThumbnailAsync(song.Id).Result;
+		}
+	}
+
 	public class YouTubeService
 	{
 		private YouTubeMusicClient _youtubeMusicClient;
---
+++ MusicApp/MusicApp/Views/ThumbnailConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace MusicApp.Views
+{
+	public class ThumbnailConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is string songId)
+			{
+				return GetThumbnailAsync(songId);
+			}
+			return null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		private async Task<BitmapImage> GetThumbnailAsync(string songId)
+		{
+			return await ThumbnailHelper.GetLowQualityThumbnailAsync(songId);
+		}
+	}
+}
